a7a59d8ebb98a1a0d0fcd9d4d282e0dbf98d88da
Rita Ogedegbe <ritaogedegbe@Ritas-MacBook-Pro.local>
2025-11-17 18:33:39 -0700

    server component

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,9 +1,19 @@
-import Navigation from "../components/Navigation";
+import Counter from "../components/Counter";
 
-export default function Page() {
+export default async function Page() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users = await res.json();
+
+  console.log("users", users);
   return (
     <div>
       <h1>Cabins Page</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+      <Counter users={users} />
     </div>
   );
 }
