edc51b5eda4a0981162ce290570815df3b865c7a
Rita Ogedegbe <ritaogedegbe@Ritas-MacBook-Pro.local>
2026-01-13 16:52:04 -0700

    clean files

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,19 +1,7 @@
-import Counter from "../_components/Counter";
-
 export default async function Page() {
-  const res = await fetch("https://jsonplaceholder.typicode.com/users");
-  const users = await res.json();
-
-  console.log("users", users);
   return (
     <div>
       <h1>Cabins Page</h1>
-      <ul>
-        {users.map((user) => (
-          <li key={user.id}>{user.name}</li>
-        ))}
-      </ul>
-      <Counter users={users} />
     </div>
   );
 }
