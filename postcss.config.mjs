7ea24140b9a45cc17e8fd01776700fdf472ac626
Rita Ogedegbe <ritaogedegbe@Ritas-MacBook-Pro.local>
2026-01-21 17:06:47 -0700

    style navigation

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,9 +1,9 @@
 import tailwindcss from "tailwindcss";
 import autoprefixer from "autoprefixer";
 
-/** @type {import('postcss-load-config').Config} */
-const config = {
-  plugins: [tailwindcss(), autoprefixer()],
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 };
-
-export default config;
