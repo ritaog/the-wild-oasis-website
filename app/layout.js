f32fb2fb97ba5621bd63bc5aab8746b4c86cf920
Rita Ogedegbe <ritaogedegbe@Ritas-MacBook-Pro.local>
2026-01-26 16:15:49 -0700

    style naviagtion and body

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,4 @@
-import Logo from "@/app/_components/Logo";
-import Navigation from "@/app/_components/Navigation";
+import Header from "@/app/_components/Header";
 import { Josefin_Sans } from "next/font/google";
 
 import "@/app/_styles/globals.css";
@@ -22,15 +21,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen`}
+        className={`${josefin.className} bg-primary-950 text-primary-100 flex min-h-screen flex-col antialiased`}
       >
-        <header>
-          <Logo />
-          <Navigation />
-        </header>
-
-        <main>{children}</main>
-        <footer>Copyright by The Wild Oasis</footer>
+        <Header />
+        <div className="flex-1 px-8 py-12">
+          <main className="mx-auto max-w-7xl">{children}</main>
+        </div>
       </body>
     </html>
   );
