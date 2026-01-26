f8c62f8588a87cc2118aedc58cf7c5eb9f405457
Rita Ogedegbe <ritaogedegbe@Ritas-MacBook-Pro.local>
2026-01-26 16:28:35 -0700

    optimize image

--- app/_components/Logo.js
+++ app/_components/Logo.js
@@ -1,9 +1,11 @@
 import Link from "next/link";
+import Image from "next/image";
+import logo from "@public/logo.png";
 
 function Logo() {
   return (
     <Link href="/" className="z-10 flex items-center gap-4">
-      <img src="/logo.png" height="60" width="60" alt="The Wild Oasis logo" />
+      <Image src="/logo.png" height="60" width="60" alt="The Wild Oasis logo" />
       <span className="text-primary-100 text-xl font-semibold">
         The Wild Oasis
       </span>
