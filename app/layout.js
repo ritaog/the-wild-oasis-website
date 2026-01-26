96665d07345700d4dd67f4068268cd9a35dfd7e3
Rita Ogedegbe <ritaogedegbe@Ritas-MacBook-Pro.local>
2026-01-26 16:48:04 -0700

    design home page

--- app/layout.js
+++ app/layout.js
@@ -21,7 +21,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${josefin.className} bg-primary-950 text-primary-100 flex min-h-screen flex-col antialiased`}
+        className={`${josefin.className} bg-primary-950 text-primary-100 relative flex min-h-screen flex-col antialiased`}
       >
         <Header />
         <div className="flex-1 px-8 py-12">
--- app/page.js
+++ app/page.js
@@ -1,12 +1,30 @@
 import Link from "next/link";
-import Navigation from "./_components/Navigation";
+import Image from "next/image";
+import bg from "@/public/bg.png";
 
 export default function Page() {
   return (
-    <div>
-      <h1>The wild Oasis. Welcome to paradise.</h1>
+    <main className="mt-24">
+      <Image
+        src={bg}
+        fill
+        className="object-cover object-top"
+        placeholder="blur"
+        quality={80}
+        alt="Mountains and forests with two cabins"
+      />
 
-      <Link href="/cabins">Explore Luxury Cabins</Link>
-    </div>
+      <div className="relative z-10 text-center">
+        <h1 className="text-primary-50 mb-10 text-8xl font-normal tracking-tight">
+          Welcome to paradise.
+        </h1>
+        <Link
+          href="/cabins"
+          className="bg-accent-500 text-primary-800 hover:bg-accent-600 px-8 py-6 text-lg font-semibold transition-all"
+        >
+          Explore luxury cabins
+        </Link>
+      </div>
+    </main>
   );
 }
