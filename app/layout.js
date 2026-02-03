ed96612e7cee49148c4eb2e74dcbdad88a4c5c51
Rita Ogedegbe <ritaogedegbe@Ritas-MacBook-Pro.local>
2026-02-02 17:42:37 -0700

    create nested layout

--- app/layout.js
+++ app/layout.js
@@ -21,11 +21,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${josefin.className} bg-primary-950 text-primary-100 relative flex min-h-screen flex-col antialiased`}
+        className={`${josefin.className} relative flex min-h-screen flex-col bg-primary-950 text-primary-100 antialiased`}
       >
         <Header />
-        <div className="flex-1 px-8 py-12">
-          <main className="mx-auto max-w-7xl">{children}</main>
+        <div className="grid flex-1 px-8 py-12">
+          <main className="mx-auto w-full max-w-7xl">{children}</main>
         </div>
       </body>
     </html>
