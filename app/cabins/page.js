53b280f58b0ffc29463becb539e570f08ff56e33
Rita Ogedegbe <ritaogedegbe@yahoo.co.uk>
2026-04-06 16:32:49 -0600

    fetch cabin data

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,19 +1,19 @@
 import CabinCard from "@/app/_components/CabinCard";
+import { getCabins } from "../_lib/data-service";
 
 export const metadata = {
   title: "Cabins",
 };
 
-export default function Page() {
-  // CHANGE
-  const cabins = [];
+export default async function Page() {
+  const cabins = await getCabins();
 
   return (
     <div>
-      <h1 className="text-accent-400 mb-5 text-4xl font-medium">
+      <h1 className="mb-5 text-4xl font-medium text-accent-400">
         Our Luxury Cabins
       </h1>
-      <p className="text-primary-200 mb-10 text-lg">
+      <p className="mb-10 text-lg text-primary-200">
         Cozy yet luxurious cabins, located right in the heart of the Italian
         Dolomites. Imagine waking up to beautiful mountain views, spending your
         days exploring the dark forests around, or just relaxing in your private
