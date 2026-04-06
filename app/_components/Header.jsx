ec6b77516fa8450c56196f0f4c820a038854ddea
Rita Ogedegbe <ritaogedegbe@Ritas-MacBook-Pro.local>
2026-04-06 15:41:19 -0600

    set up supabase

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -2,8 +2,9 @@ import Navigation from "@/app/_components/Navigation";
 import Logo from "@/app/_components/Logo";
 
 function Header() {
+  console.log("header...");
   return (
-    <header className="border-primary-900 border-b px-8 py-5">
+    <header className="border-b border-primary-900 px-8 py-5">
       <div className="mx-auto flex max-w-7xl items-center justify-between">
         <Logo />
         <Navigation />
